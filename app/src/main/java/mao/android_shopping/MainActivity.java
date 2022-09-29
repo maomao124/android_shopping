3fb33b0d63d7faf66ed3865685a235f0bea1211b
mao <1296193245@qq.com>
2022-09-29 19:46:05 +0800

    android_shopping

---
+++ app/src/main/java/mao/android_shopping/MainActivity.java
@@ -0,0 +1,16 @@
+package mao.android_shopping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
